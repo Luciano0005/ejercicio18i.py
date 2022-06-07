cba52b5249610453172ee4f3104396e1c32a6cf7
Luciano0005 <luc.perez@duocuc.cl>
2022-06-07 11:58:31 -0400

    agregar py

---
+++ ejercicio18i.py
@@ -0,0 +1,10 @@
+'''
+#Crear un arreglo de dos dimensiones de tamaño 3 x 3,
+con elementos aleatorios de números enteros del 0 al 100.
+'''
+
+import numpy as np
+import random as rd
+
+matriz = np.diag([1,1,1])
+print(matriz)
